bae4c30e2d2aaef3628b24caaaacc36c2649fe7d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-23 14:26:01 +0400

    add for in

--- L8/class-work/app.js
+++ L8/class-work/app.js
@@ -2,22 +2,22 @@
 // 2. qiymeti 1000 azn asagi sal! {14-cu setirdeki kimi}
 
 
-let car = {
-    model: 'BMW',
-    year: 2022,
-    price: 70000,
+// let car = {
+//     model: 'BMW',
+//     year: 2022,
+//     price: 70000,
 
-    owner: {
-        name: "Ali",
-        age: 23,
-        job: 'programmer'
-    },
-    showInfo: () => {
-        console.log(`${car.owner.name} has ${car.model}. Its price is ${car.price}`)
-    }
-}
+//     owner: {
+//         name: "Ali",
+//         age: 23,
+//         job: 'programmer'
+//     },
+//     showInfo: () => {
+//         console.log(`${car.owner.name} has ${car.model}. Its price is ${car.price}`)
+//     }
+// }
 
-car.showInfo()
+// car.showInfo()
 
 // let endirim = prompt('Ne qeder endirim isdiyisen?');
 // car.price = car.price - endirim;
@@ -27,5 +27,22 @@ car.showInfo()
 // owner: {name, age, job}
 // car.showInfo() // Ali has bmw. Its price is 70000
 // _______________________________________________________________________________________________________________________________________
+
+
+
+
+// const car = {
+//     model: "BmW",
+//     year: 2022,
+//     price: 70000,
+//     country: "Germany",
+//     owenerName: "Ruslan",
+//     owenerAge: 23,
+//     owenerNationality: "Azerbaijani",
+// };
+
+// for (let key in car) {
+//     console.log(`${key} - ${car[key]}`)
+// }
 
 
