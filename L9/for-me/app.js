52767ff78c516e72b981ddd61483e3a71e5c51b9
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-23 16:48:08 +0400

    add data-type

---
+++ L9/for-me/app.js
@@ -0,0 +1,81 @@
+// ehmed
+// ali
+
+// let a = 'ali'
+// let b = a
+// a = 'ahmad'
+
+// console.log(a)     //ahmad
+// console.log(b)     //ali     
+
+// let obj = {ad: "ali"}
+// let obj2 = obj
+
+// obj.ad = 'ahmad'
+// console.log(obj.ad)        //ahmad 
+// console.log(obj2.ad)       //ahmad
+
+
+/*
+
+
+data type 2 yere bolunur:
+1. value type [primitive type]: number, boolean, string, undefined
+2.referance type [non-primitive type]: object (function, array)
+
+
+*/
+
+
+
+
+//-------------------------------------------------------------------example--------------------------------------------------------------
+
+// function changeName(ad) {
+//     ad = "ali"
+// }
+
+// let name_ = "ahmad"
+// changeName(name_);
+// console.log(name_);   //deyesmedi
+
+// ---
+
+// function changeNameObj(obj) {
+//     obj.ad = "ali"
+// }
+// let key = { ad: "ahmad" }
+// changeNameObj(key);
+// console.log(key.ad);
+
+
+
+// __________________________________________________________________________________________________________________________________________
+
+
+
+
+
+
+// function cemWithCallback(a, b, callbak) {
+//     callbak(a + b);
+// }
+
+// cemWithCallback(12, 23, console.log)
+
+
+
+
+// __________________________________________________________________________________________________________________________________________
+
+// function changeNameObj(obj3) {
+//     console.log(obj3 === obj)
+//     obj3.ad = 'ali'
+
+// }
+
+// let obj = { ad: 'ahmad' }
+// changeNameObj(obj)
+// console.log(obj.ad)
+
+// imumi baxdiqlarimiz / 1. kopyalama, 2.funksiya verilme, 3.ifadeler 
