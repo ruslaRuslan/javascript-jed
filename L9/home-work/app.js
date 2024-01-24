512678b14e79f818b35fe8c060a9511610d37588
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-24 13:00:19 +0400

    add task_02

--- L9/home-work/app.js
+++ L9/home-work/app.js
@@ -1,13 +1,29 @@
 
 //-------------------------------------------------------------------------------------task-01--------------------------------------------------------------------------- 
-function prop2Taker(obj) {
-    if ('prop-2' in obj) {
-        return obj['prop-2'];
-    }
-}
+// function prop2Taker(obj) {
+//     if ('prop-2' in obj) {
+//         return obj['prop-2'];
+//     }
+// }
 
-console.log(prop2Taker({ one: 1, 'prop-2': 2 }));  // 2
-console.log(prop2Taker({ 'prop-2': 'two', prop: 'test' }));
+// console.log(prop2Taker({ one: 1, 'prop-2': 2 }));  
+// console.log(prop2Taker({ 'prop-2': 'two', prop: 'test' }));
+
+// ______________________________________________________________________________________________________________________________________________________________________
+
+
+//-------------------------------------------------------------------------------------task-02--------------------------------------------------------------------------- 
+
+// function propertyTaker(obj, propertyName) {
+//     if (propertyName in obj) {
+//         return obj[propertyName];
+//     }
+// }
+
+// console.log(propertyTaker({ country: 'Japan',  continent: 'Asia' }, 'continent'));  
+// console.log(propertyTaker({ country: 'Sweden', continent: 'Europe' }, 'country'));  
+// console.log(propertyTaker({ name: 'Ali', age: 12 }, 'name'));  
+
 // ______________________________________________________________________________________________________________________________________________________________________
 
 
