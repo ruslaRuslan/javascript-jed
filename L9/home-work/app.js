bc83415edd9b897c1328bf67b893a33b71fc3ffc
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-24 14:13:26 +0400

    add

--- L9/home-work/app.js
+++ L9/home-work/app.js
@@ -1,12 +1,10 @@
 
 //-------------------------------------------------------------------------------------task-01--------------------------------------------------------------------------- 
-// function prop2Taker(obj) {
-//     if ('prop-2' in obj) {
-//         return obj['prop-2'];
-//     }
+// function prop2Taker(obj) { 
+//         return obj['prop-2']; 
 // }
 
-// console.log(prop2Taker({ one: 1, 'prop-2': 2 }));  
+// console.log(prop2Taker({ ad: 1, 'prop-2': 2 }));  
 // console.log(prop2Taker({ 'prop-2': 'two', prop: 'test' }));
 
 // ______________________________________________________________________________________________________________________________________________________________________
@@ -14,15 +12,12 @@
 
 //-------------------------------------------------------------------------------------task-02--------------------------------------------------------------------------- 
 
-// function propertyTaker(obj, propertyName) {
-//     if (propertyName in obj) {
-//         return obj[propertyName];
-//     }
-// }
+// propertyTaker = (obj, propertyName) => obj[propertyName];
 
-// console.log(propertyTaker({ country: 'Japan',  continent: 'Asia' }, 'continent'));  
-// console.log(propertyTaker({ country: 'Sweden', continent: 'Europe' }, 'country'));  
-// console.log(propertyTaker({ name: 'Ali', age: 12 }, 'name'));  
+
+// console.log(propertyTaker({ country: 'Japan', continent: 'Asia' }, 'continent'));
+// console.log(propertyTaker({ country: 'Sweden', continent: 'Europe' }, 'country'));
+// console.log(propertyTaker({ name: 'Ali', age: 12 }, 'name'));
 
 // ______________________________________________________________________________________________________________________________________________________________________
 
@@ -31,14 +26,8 @@
 
 //-------------------------------------------------------------------------------------task-03--------------------------------------------------------------------------- 
 
-// function existAndTruthy(obj, propertyName) {
-//     if (propertyName in obj && obj[propertyName]) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
+//  existAndTruthy = (obj, propertyName) => propertyName in obj && obj[propertyName] 
+
 
 // console.log(existAndTruthy({ a: 1, b: 2, c: 3 }, 'b'));
 // console.log(existAndTruthy({ x: 'a', y: null, z: 'c' }, 'y'))
