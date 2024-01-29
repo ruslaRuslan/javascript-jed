054cb07f092c90f5ab6b2fdde9a2782797641303
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-29 16:53:45 +0400

    add

--- L9/class-work/app.js
+++ L9/class-work/app.js
@@ -40,11 +40,24 @@
 //   console.log(myFunction('z', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 0
 // _______________________________________________________________________________________________________________________________________________________________________
   
+// function myFunction(a) {
+//     return Math.round(a * 100) / 100;
+//   }
+  
+//   console.log(myFunction(2.12397)); // Ожидаемый результат: 2.12
+//   console.log(myFunction(3.136));   // Ожидаемый результат: 3.14
+//   console.log(myFunction(1.12397)); // Ожидаемый результат: 1.12
+//   console.log(myFunction(26.1379)); // Ожидаемый результат: 26.14
+// _______________________________________________________________________________________________________________________________________________________________________
+
+
 function myFunction(a) {
-    return Math.round(a * 100) / 100;
+    return Array.from(String(a), Number);
   }
   
-  console.log(myFunction(2.12397)); // Ожидаемый результат: 2.12
-  console.log(myFunction(3.136));   // Ожидаемый результат: 3.14
-  console.log(myFunction(1.12397)); // Ожидаемый результат: 1.12
-  console.log(myFunction(26.1379)); // Ожидаемый результат: 26.14
+  console.log(myFunction(10));      // Ожидаемый результат: [1, 0]
+  console.log(myFunction(931));     // Ожидаемый результат: [9, 3, 1]
+  console.log(myFunction(193278));  // Ожидаемый результат: [1, 9, 3, 2, 7, 8]
+  
+
+
