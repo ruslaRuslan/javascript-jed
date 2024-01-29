2c19f57e2d917bc09bcfc3e35bde9fd3f539dc7b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-29 16:37:08 +0400

    add

--- L9/class-work/app.js
+++ L9/class-work/app.js
@@ -19,13 +19,23 @@
 
 
 
+// function myFunction(a, b) {
+//     return a.indexOf(b) !== -1 ? b + a : a + b;
+// }
+
+// console.log(myFunction('cheese', 'cake')); // Ожидаемый результат: 'cheesecake'
+// console.log(myFunction('lips', 's')); // Ожидаемый результат: 'slips'
+// console.log(myFunction('Java', 'script')); // Ожидаемый результат: 'Javascript'
+// console.log(myFunction(' think, therefore I am', 'I')); // Ожидаемый результат: 'I think, therefore I am'
+
+// _______________________________________________________________________________________________________________________________________________________________________
+
 function myFunction(a, b) {
-    return a.indexOf(b) !== -1 ? b + a : a + b;
-}
-
-console.log(myFunction('cheese', 'cake')); // Ожидаемый результат: 'cheesecake'
-console.log(myFunction('lips', 's')); // Ожидаемый результат: 'slips'
-console.log(myFunction('Java', 'script')); // Ожидаемый результат: 'Javascript'
-console.log(myFunction(' think, therefore I am', 'I')); // Ожидаемый результат: 'I think, therefore I am'
-
-
+    return b.split(a).length - 1;
+  }
+  
+  console.log(myFunction('m', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 2
+  console.log(myFunction('h', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 4
+  console.log(myFunction('?', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 1
+  console.log(myFunction('z', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 0
+  
