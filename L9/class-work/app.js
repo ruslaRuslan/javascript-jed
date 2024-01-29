f84e0673acb6b66522a43ad2c5cd133d1c968dd6
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-29 16:45:15 +0400

    add

--- L9/class-work/app.js
+++ L9/class-work/app.js
@@ -30,12 +30,21 @@
 
 // _______________________________________________________________________________________________________________________________________________________________________
 
-function myFunction(a, b) {
-    return b.split(a).length - 1;
+// function myFunction(a, b) {
+//     return b.split(a).length - 1;
+//   }
+  
+//   console.log(myFunction('m', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 2
+//   console.log(myFunction('h', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 4
+//   console.log(myFunction('?', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 1
+//   console.log(myFunction('z', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 0
+// _______________________________________________________________________________________________________________________________________________________________________
+  
+function myFunction(a) {
+    return Math.round(a * 100) / 100;
   }
   
-  console.log(myFunction('m', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 2
-  console.log(myFunction('h', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 4
-  console.log(myFunction('?', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 1
-  console.log(myFunction('z', 'how many times does the character occur in this sentence?')); // Ожидаемый результат: 0
-  
+  console.log(myFunction(2.12397)); // Ожидаемый результат: 2.12
+  console.log(myFunction(3.136));   // Ожидаемый результат: 3.14
+  console.log(myFunction(1.12397)); // Ожидаемый результат: 1.12
+  console.log(myFunction(26.1379)); // Ожидаемый результат: 26.14
