7d351355897c0cfcacf6fc9344929d79d0a6623a
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-01-31 14:10:19 +0400

    add

--- L9/home-work/app.js
+++ L9/home-work/app.js
@@ -6,7 +6,7 @@
 const car = {
     model: 'zapi',
     il: 1969,
-    olke: 'Azerbaycan'
+    olke: 'Russiya'
 }
 
 car2 = { ...car }
@@ -15,56 +15,70 @@ car2 = {
     model: 'BMW',
     il: 2024,
 }
-console.log(car.model, car.il)
-console.log(car2.model, car.il)
+console.log(car)
+console.log(car2)
 
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
 // T2. Bir Arrow  funksiya yarat. Ona car obyektini verim. O console-a bele bir mesaj cixartsin:
 // showInfo(car)
 // -> zapi markali masin, 1969-da russiya-da istehsal olunub.
 
+const showInfo = (car) => {
+    car = { model: 'zapi', il: 1969, olke:'Russiya'};
+    console.log(`${car.model} markali masin, ${car.il}-da ${car.olke}-da istehsal olunub.`)
+}
+showInfo(car)
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 // T3. showAge(car) // Verilen masinin 55 yasi var.
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T4. car obyektine yeni bir property-e elave et. sahib: {ad:'Ruslan', soyad:'Zeynalov', dogumIl:2000}
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T5. showOwner(car) // zapi markali masinin sahibi Zeynalov Ruslandir. O 2000-ci ilde dogulub.
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T6. showAgeDifferences(car) // sahibinin dogum ili ile masinin il ferqi. (sahib 2000, zapi 1969) -> 31 il.
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T7*. Yeni bir car3 obyekti yarat Spread (...) operatoru ile. Sahib melumatlarini deyis. (sahib: ali, isiyev, 1995)
 
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T8. isEqual(car, car3) -> bu funksiya 2 obyekt alir, onlar beraberdirse, console-a "Bunlar eyni obyektlerdir " , eks halda "Bunlar ferqli obyektlerdir"
 
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T9. isSameName(car, car2) -> bu funksiya 2 obyekt alir, obyektlerin modelleri eynidirse "Bunlar eyni modeldirler", eks halda "bunlar ferqli modeldirler" mesajini console-a cixarsin.
 
 
 
 
-// -----------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 // T10. isSameOwner(car, car3)-> bu funksiya 2 obyekt alir, obyektlerin sahiblerinin adlari eynidirse "Bunlar eyni adama mexsusdur", eks halda "bunlar ferqli adamlara mexsusdur" mesajini console-a cixarsin.
 
