b594bb41fe8279a7c1defd55d0c5368c50dadda7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-02 16:09:31 +0400

    escape character

--- L10/for-me/app.js
+++ L10/for-me/app.js
@@ -1,3 +1,12 @@
-let ad = `2+2=${2 + 2}
-2*3=${2 * 3}`
-console.log(ad)
+// let ad = `2+2=${2 + 2}
+// 2*3=${2 * 3}`
+// console.log(ad)
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// escape character (special)  \n , \t , \a
+let ad = 'ali\t\tahmad'
+let ad2 = 'ali\tahmad'
+let ad3 = 'ali\\ahmad'   // iki dene \\ beraberdi bir \, \\===\
+let ad4 = '\u{1F60D}'
+
+console.log(`1.  ${ad},  |  2. ${ad2}, |  3. ${ad3}, | 4. ${ad4} `)
