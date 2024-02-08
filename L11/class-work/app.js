d7199cdf366e01897e5567a3522216495b0b3ae7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-08 15:10:45 +0400

    arrays function added

--- L11/class-work/app.js
+++ L11/class-work/app.js
@@ -32,18 +32,50 @@
 // const friends = ['ali', 'ruslan', 'renat', 'ramin', 'idris']
 
 // T1. 'renat' elementinin index-ni tap;
+// function findFriends(name) {
+//     const friends = ['ali', 'ruslan', 'renat', 'ramin', 'idris']
+//     let index = friends.indexOf(name)
+//     console.log(index);
+//     return index
+// }
+// findFriends('renat')
+// _____________________________________________________________
 
+// T2. 'idris' elementi bu massive daxildirse, ekrana "Idris bizim dostumuzdur" cumlesini cixar. Yoxdursa, "Idris abi burda yoxdur" cixart.
 
-// T2. 'idris' elementi bu massive daxildirse, ekrana "Idris bizim dostumuzdur" cumlesini cixar. Yoxdursa, "Idris abi burda yoxdur" cixart
+// function findFriends(name) {
+//     const friends = ['ali', 'ruslan', 'renat', 'ramin', 'idris']
+//     if (name === (friends[4])) {
+//         return `${friends[4]} bizim dostumuzdur`;
+//     }
+//     return `${friends[4]} abi burda yoxdur`
+// }
+// console.log(findFriends('renat'));
+// console.log(findFriends('ali'));
+// console.log(findFriends('idris'));
 
 
 // T3. concat funksiyasi vasitesile bura yeni dostlar elave et: samil, sada Bunu yeni massive menimset -> friends2
 
+// function findFriends2() {
+//     let name = 'samil';
+//     let result = name.concat('sada');
+//     console.log(result);
+//     return result;
+// }
+// findFriends2();
+
 
 // T4. massivden ortadaki 2 elementi gotur (slice ile) (renat ve ramin elementlerini)
 
+// const friends = ['ali', 'ruslan', 'renat', 'ramin', 'idris']
+// let result = friends.slice(2, 4);
+// console.log(result);
+
 
 // T5. bu massivle verilen massivi birlesdir (concat vasitesile)
+const friends = ['ali', 'ruslan', 'renat', 'ramin', 'idris'];
+const enemies = ['alion', 'greenheart', 'javascript', 'react'];
+let result = friends.concat(enemies)
+console.log(result);
 
-
-// const enemies = ['alion', 'greenheart', 'javascript', 'react']
