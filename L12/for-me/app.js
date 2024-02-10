45d7bec6f35109b1c5eeb4695afe38fa34bdbb4e
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-10 14:38:42 +0400

    aski tablede-siralama isi gordum

--- L12/for-me/app.js
+++ L12/for-me/app.js
@@ -4,10 +4,20 @@
 //----------------------------------------------------------------------------------------------------------------------------------------
 
 // array-in metodlari
-let arr = ['test', 'sada', 'ali', 'sahmurad', 'Ali', 'ruslan', 'abbas', 'intiqam', 'bayram']
+// let arr = ['test', 'sada', 'ali', 'sahmurad', 'Ali', 'ruslan', 'abbas', 'intiqam', 'bayram']
 
-arr.sort()  // elifba sirasina gore duzur 
-console.log(arr); //['Ali', 'abbas', 'ali', 'bayram', 'intiqam', 'ruslan', 'sada', 'sahmurad', 'test']
+// arr.sort()  // elifba sirasina gore duzur 
+// console.log(arr); //['Ali', 'abbas', 'ali', 'bayram', 'intiqam', 'ruslan', 'sada', 'sahmurad', 'test']
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// array-in metodlari   
+// let arr = ['test', 'sada', 'ali', 'sahmurad', 'Ali', 'Ruslan', 'abbas', 'intiqam', 'bayram']
+
+// arr.sort()  // elifba sirasina gore duzur 
+// console.log(arr); //['Ali', 'Ruslan', 'abbas', 'ali', 'bayram', 'intiqam', 'sada', 'sahmurad', 'test']
+
+// asky table-da balaca a 97-di, boyuk A 65-di. 65 vakum seraitinde kicikdi 97-den. Aski cedvelindeki nomresi kicik eded qabaga dusur
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 
