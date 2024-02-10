02b8ad4e41048bef269f38d13d207a775d3a0c7e
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-10 16:06:00 +0400

    forEach vasitesi ile adini ve indexsini tapdim

--- L12/for-me/app.js
+++ L12/for-me/app.js
@@ -80,8 +80,22 @@ arr = arr.map((elem, index, arr)=>) arrayi deyismek ucun bele etmelisen!
 
 // 1. arr.forEach((elem, index, arr)=>)
 // Massivin uzerinden loop etmek:
+// let arr = ['ali', 'sada', 'sah', 'ferid']
+// arr.forEach((ad, index, arr) => {
+//     console.log(ad, index, arr); })// console-nin cavabi asagida 
+                                           //     ↓
+
+//     ali 0 (4) ['ali', 'sada', 'sah', 'ferid']
+// app.js:85 sada 1 (4) ['ali', 'sada', 'sah', 'ferid']0: "ali"1: "sada"2: "sah"3: "ferid"length: 4[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+// app.js:85 sah 2 (4) ['ali', 'sada', 'sah', 'ferid']0: "ali"1: "sada"2: "sah"3: "ferid"length: 4[[Prototype]]: Array(0)
+// app.js:85 ferid 3 (4) ['ali', 'sada', 'sah', 'ferid']
+
+// ___________________________________________________
 let arr = ['ali', 'sada', 'sah', 'ferid']
 arr.forEach((ad, index, arr) => {
-    console.log(ad, index, arr);
+    console.log(ad, index);  //      ↓
 })
-
+                                   //ali 0
+                                   //sada 1
+                                   //sah 2
+                                   //ferid 3
