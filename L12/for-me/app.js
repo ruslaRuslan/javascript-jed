76ef0415232ff4cea54bb854f73847493542d274
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-10 14:13:12 +0400

    array-in metodlari. Arrayleri elifba sirasina gore duzdum.

--- L12/for-me/app.js
+++ L12/for-me/app.js
@@ -1,4 +1,15 @@
-let arr = ['test', 'sada', 'ali', 'sahmurad', 'ali', 'intiqam']
-arr[0] = 'ali'
-console.log(arr); //['ali', 'sada', 'ali', 'sahmurad', 'ali', 'intiqam']
+// let arr = ['test', 'sada', 'ali', 'sahmurad', 'ali', 'intiqam']
+// arr[0] = 'ali'
+// console.log(arr); //['ali', 'sada', 'ali', 'sahmurad', 'ali', 'intiqam']
 //----------------------------------------------------------------------------------------------------------------------------------------
+
+// array-in metodlari
+let arr = ['test', 'sada', 'ali', 'sahmurad', 'Ali', 'ruslan', 'abbas', 'intiqam', 'bayram']
+
+arr.sort()  // elifba sirasina gore duzur 
+console.log(arr); //['Ali', 'abbas', 'ali', 'bayram', 'intiqam', 'ruslan', 'sada', 'sahmurad', 'test']
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
