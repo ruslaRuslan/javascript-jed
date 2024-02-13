d95a5fb3ede0db76c08210f7c6d20d956fe5dece
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-13 14:47:52 +0400

    daha qisa formasi

--- L12/class-work/app.js
+++ L12/class-work/app.js
@@ -96,7 +96,6 @@ arr = arr.map((elem, index, arr)=>) arrayi deyismek ucun bele etmelisen!
 // _______________________________________________________________________________________________________________________________________
 
 const fe10 = ['ali', 'muhtar', 'sada', 'sabina']
-const result = fe10.every((elem, ind) => {
-    return elem.length < 6 ;
-})
+const result = fe10.every((elem, ind) => elem.length < 6)
 console.log(result);
+// _______________________________________________________________________________________________________________________________________
