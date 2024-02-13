5d058134b1c12ad41f6beb0adaf4eb2ad17281fe
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-13 14:59:14 +0400

    sadece yoxladix onceki kimi

--- L12/class-work/app.js
+++ L12/class-work/app.js
@@ -111,4 +111,9 @@ arr = arr.map((elem, index, arr)=>) arrayi deyismek ucun bele etmelisen!
 // console.log(result);   //true
 
 // _______________________________________________________________________________________________________________________________________
- 
+
+
+// falsy 0, Nan, '', false, undefined, null
+const fe10 = ['ali', 'muhtar', 'sada', 'sabina', ' ']
+const result = fe10.every((elem, ind) => 2)
+console.log(result); 
