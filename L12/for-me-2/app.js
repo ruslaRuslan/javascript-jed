85447c7206b7242684fe6a39be33b5d9990f374e
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-14 17:11:38 +0400

    forEach() vasitesi ile index-i ve adi console cixartdim

--- L12/for-me-2/app.js
+++ L12/for-me-2/app.js
@@ -34,7 +34,7 @@
 // }
 // console.log(showNegatives(arr)); // [-1, -4, -8, -7, -4]
 
-// _______________________________________________________________________________________________________________________________________
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
 // 2. Adlar massivini parametr kimi qebul eden bir funksiyada yazin, bele ki, o funksiyadan  bize en uzun adi cixaracax.
 // mes: // let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles'] 
@@ -47,24 +47,36 @@
 //     massiv.sort((a, b) => b.length - a.length)
 //     return massiv[0];
 // }
+// console.log(findLongestName(arr));
+
+
+// _______________________________________________________________________________________________________________________________________
+
+// let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
+
+// const findLongestName = (massiv) => {
+
+//     let result_Uzun_ad = ''
+//     massiv.forEach((name) => {
+//         if (name.length > result_Uzun_ad.length) {
+//             result_Uzun_ad = name;
+//         }
+//     });
+
+//     return result_Uzun_ad;
+// }
 // console.log(findLongestName(arr))
 
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
-// _______________________________________________________________________________________________________________________________________
+// 3. const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"]; massivi verilib. printWithIndex adli funksiya yaradin ve onu names.forEach methoduna callback kimi verin. Ekrana adin index-i ve adi cixarsin. (0-Michael, 1-Trevor ve.s...)
 
-let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
-
-const findLongestName = (massiv) => {
-
-    let result_Uzun_ad = ''
-    massiv.forEach((name) => {
-        if (name.length > result_Uzun_ad.length) {
-            result_Uzun_ad = name
-        }
-    });
-
-    return result_Uzun_ad;
+const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"]
+const printWithIndex = (name, index) =>{
+    console.log(`${index} - ${name}`);
 }
-console.log(findLongestName(arr))
+names.forEach(printWithIndex);
 
 // _______________________________________________________________________________________________________________________________________
+
+
