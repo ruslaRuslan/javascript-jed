256f3275671ef05c8f2799838f263a69d95ed6ce
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-14 14:17:20 +0400

    massivde olan butun menfi reqemleri massiv halinda cevirip console cixartdim

--- L12/for-me-2/app.js
+++ L12/for-me-2/app.js
@@ -1,17 +1,31 @@
+// let arr = [1, -1, 2, 5, 7, -4, 8, -8, -7, -4]
+// // forEach, if
+
+// function showNegatives(massiv) {
+//     massiv.forEach((element) => {
+//         if (element < 0) {
+//             console.log(element);
+//         }
+//     });
+// }
+// showNegatives(arr)
+// _______________________________________________________________________________________________________________________________________
+
 let arr = [1, -1, 2, 5, 7, -4, 8, -8, -7, -4]
 // forEach, if
 
 function showNegatives(massiv) {
-    massiv.forEach(element => {
+    let array = []
+    massiv.forEach((element) => {
         if (element < 0) {
-            console.log(element);
+            array.push(element)
         }
     });
+    return array;
 }
-showNegatives(arr)
-// ---------------------------------------------------------------------------------------------------------------------------------------
+console.log(showNegatives(arr));
 
-
+// _______________________________________________________________________________________________________________________________________
 
 
 
