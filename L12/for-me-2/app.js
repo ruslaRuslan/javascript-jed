4717c2fb6e03f9f54312fac336c3d2c5e71b78de
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-14 19:43:42 +0400

    sade numune gosdermek ucun

--- L12/for-me-2/app.js
+++ L12/for-me-2/app.js
@@ -12,10 +12,10 @@
 
 // ↓
 //-1
-//-4 
+//-4
 //-8
 //-7
-//-4 
+//-4
 
 
 // _______________________________________________________________________________________________________________________________________
@@ -37,7 +37,7 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
 // 2. Adlar massivini parametr kimi qebul eden bir funksiyada yazin, bele ki, o funksiyadan  bize en uzun adi cixaracax.
-// mes: // let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles'] 
+// mes: // let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
 // findLongestName(arr); // 'muhammad'
 
 
@@ -47,7 +47,7 @@
 //     massiv.sort((a, b) => b.length - a.length)
 //     return massiv[0];
 // }
-// console.log(findLongestName(arr));
+// console.log(findLongestName(arr));  //muhammad
 
 
 // _______________________________________________________________________________________________________________________________________
@@ -65,7 +65,7 @@
 
 //     return result_Uzun_ad;
 // }
-// console.log(findLongestName(arr))
+// console.log(findLongestName(arr)) //muhammad
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
@@ -76,6 +76,12 @@
 //     console.log(`${index} - ${name}`);
 // }
 // names.forEach(printWithIndex);
+
+// 0 - Michael
+// 1 - Trevor
+// 2 - Franklin
+// 3 - Lamar
+// 4 - Jimmy
 
 // _______________________________________________________________________________________________________________________________________
 
@@ -90,6 +96,12 @@
 // }
 
 // names.forEach(printWithDashes)
+
+// --- Michael --- 
+// --- Trevor --- 
+// --- Franklin --- 
+// --- Lamar --- 
+// --- Jimmy --- 
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
@@ -122,13 +134,13 @@
 //         return element * 2
 //     }
 // })
-
+// console.log(result) //[2, 10, 7, 6, 9, 8, 6, 4]
 
 // _______________________________________________________________________________________________________________________________________
 
 // let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
 // const result = numbers.map((eded) => eded < 0 ? eded * -1 : eded * 2)
-// console.log(result);
+// console.log(result); //[2, 10, 7, 6, 9, 8, 6, 4]
 
 // _______________________________________________________________________________________________________________________________________
 
@@ -136,9 +148,9 @@
 
 // let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
 // const result = numbers.map((eded) => eded < 0 ? Math.abs(eded) : eded * 2)
-// console.log(result);
+// console.log(result); //[2, 10, 7, 6, 9, 8, 6, 4]
 
-// _______________________________________________________________________________________________________________________________________
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
 // 6. A herfi ile baslayan orijinal massivdeki elemenlerden ibaret yeni massiv yaradin: let names = ['Sam','Alan', 'Bill', 'Adam', 'Anna', 'George'] // ['Alan', 'Adam', 'Anna']
 
@@ -152,10 +164,51 @@
 // _______________________________________________________________________________________________________________________________________
 
 
-let names = ['Sam', 'Alan', 'Bill', 'Adam', 'Anna', 'George'];
+// let names = ['Sam', 'Alan', 'Bill', 'Adam', 'Anna', 'George'];
 
-const result = names.filter((element) => element[0] === 'A'
-)
-console.log(result); //['Alan', 'Adam', 'Anna']
+// const result = names.filter((element) => element[0] === 'A'
+// )
+// console.log(result); //['Alan', 'Adam', 'Anna']
 
 
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// 7. Ededler massivindeki tek ve cut ededleri saymaq ucun proqram yazin.let number = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84]
+// meselen, countEvensAndOdds(numbers); // evens: 11, odds: 7
+
+// let numbers = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84]
+
+// const countEvensAndOdds = (massiv) => {
+
+//     let tekEdedlerSayi = 0;
+//     let cutEdedlersayi = 0;
+//     massiv.forEach((number) => {
+//         if (number % 2 === 0) {
+//             cutEdedlersayi++
+//         } else {
+//             tekEdedlerSayi++
+//         }
+//     });
+//     return `evens: ${cutEdedlersayi}, odds: ${tekEdedlerSayi}`
+// }
+
+// console.log(countEvensAndOdds(numbers)); // evens: 11, odds: 7
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// 8. Adlar massivini parametr kimi qebul eden bir funksiyada yazin, bele ki, o funksiyadan  bize en uzun adi cixaracax.
+// mes: // let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
+// findLongestName(arr); // 'muhammad'
+
+
+// let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
+
+
+// _______________________________________________________________________________________________________________________________________
+
+// sade numune gosdermek ucun
+let numbers = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84]
+numbers.reduce((a, b) => {
+    console.log(a, b);
+    return a + b;
+}, 0)
