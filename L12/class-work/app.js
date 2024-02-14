cceba6e568fb3283c8ff50bc5c7a2a2e884bf9ed
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-14 11:21:37 +0400

    map vasitesi ile yeni massiv yaratdim ve butun massivdeki birinci herifleri boyudup console cixartdim

--- L12/class-work/app.js
+++ L12/class-work/app.js
@@ -35,7 +35,7 @@
 
 /*
 1. arrayin ES6 (esmascript 2015) methods
-forEach+, filter, find, findIndex, map, every+, some+, reduce*
+forEach+, filter+, find+, findIndex, map, every+, some+, reduce*
 
 2. hamsi: eyni arqument olaraq function yeni, callback alir:
 (ArraydekiElement, ElementIndex, MassiveReferanse)=>{}
@@ -301,6 +301,7 @@ arr = arr.map((elem, index, arr)=>) arrayi deyismek ucun bele etmelisen!
 // const task1 = fe10.find((name) => name.endsWith('i') && name.length === 4)
 // console.log(task1);
 
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
 
 // T3. Butun adlar-da a herfi varmi? [every]
@@ -310,17 +311,39 @@ arr = arr.map((elem, index, arr)=>) arrayi deyismek ucun bele etmelisen!
 // const task3 = fe10.every((name) => name.includes('a'))
 // console.log(task3);
 
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+
 // T4. Alman turist: Sizin adlarinizda z herfi isdifafe olunurmu[some]
+
+// const fe10 = ['ali', 'aslan', 'bali', 'muhtar', 'sada', 'sabina', 'aliabbas', 'alineymet', 'alisahib', 'alidadas', 'behruz']
+
+// const task4 = fe10.some((name) => name.includes('z'))
+// console.log(task4);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// const fe10 = ['ali', 'aslan', 'bali', 'muhtar', 'sada', 'sabina', 'aliabbas', 'alineymet', 'alisahib', 'alidadas', 'behruz']
+
+// // bashHerifler = ['a', 'a', 'r', 'm', 's', 'b']
+// // mapper
+// // map
+
+// const yeniArr = fe10.map((element, i) => element[0].toUpperCase() )
+// console.log(yeniArr);
+
+
+
+
 
 const fe10 = ['ali', 'aslan', 'bali', 'muhtar', 'sada', 'sabina', 'aliabbas', 'alineymet', 'alisahib', 'alidadas', 'behruz']
 
-const task4 = fe10.some((name) => name.includes('z'))
-console.log(task4);
+// bashHerifler = ['a', 'a', 'r', 'm', 's', 'b']
+// mapper
+// map
 
-
-
-
-
+const yeniArr = fe10.map((element, i) => element[0].toUpperCase() )
+console.log(yeniArr);
 
 
 
