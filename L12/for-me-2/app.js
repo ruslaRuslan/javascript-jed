3c96c5eaae1843b738c8452d995025a5c2831426
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-14 15:43:18 +0400

    massivde olan en uzu adi cixartdim(daha dogrusu teyin etdim ) sort() ile

--- L12/for-me-2/app.js
+++ L12/for-me-2/app.js
@@ -8,27 +8,47 @@
 //         }
 //     });
 // }
-// showNegatives(arr)
+// showNegatives(arr);
+
+// ↓
+//-1
+//-4 
+//-8
+//-7
+//-4 
+
+
 // _______________________________________________________________________________________________________________________________________
 
-let arr = [1, -1, 2, 5, 7, -4, 8, -8, -7, -4]
-// forEach, if
+// let arr = [1, -1, 2, 5, 7, -4, 8, -8, -7, -4]
+// // forEach, if
 
-function showNegatives(massiv) {
-    let array = []
-    massiv.forEach((element) => {
-        if (element < 0) {
-            array.push(element)
-        }
-    });
-    return array;
+// function showNegatives(massiv) {
+//     let array = []
+//     massiv.forEach((element) => {
+//         if (element < 0) {
+//             array.push(element)
+//         }
+//     });
+//     return array;
+// }
+// console.log(showNegatives(arr)); // [-1, -4, -8, -7, -4]
+
+// _______________________________________________________________________________________________________________________________________
+
+// 2. Adlar massivini parametr kimi qebul eden bir funksiyada yazin, bele ki, o funksiyadan  bize en uzun adi cixaracax.
+// mes: // let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles'] 
+// findLongestName(arr); // 'muhammad'
+
+
+let arr = ['ali', 'reza', 'aliriza', 'muhammad', 'cosqun', 'eles']
+
+const findLongestName = (massiv) => {
+    massiv.sort((a, b) => b.length - a.length)
+    return massiv[0];
 }
-console.log(showNegatives(arr));
+console.log(findLongestName(arr))
+
 
 // _______________________________________________________________________________________________________________________________________
-
-
-
-
-
 
