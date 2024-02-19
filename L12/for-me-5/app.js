f14ea08fe0d3831d1234583aafe13b89081405c9
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-19 16:53:16 +0400

    new Date().getDay()

--- L12/for-me-5/app.js
+++ L12/for-me-5/app.js
@@ -55,10 +55,113 @@
 // task03
 // verify gmail addres. (gmail addresleri sonu @gmail.com ile bitmelidir, ve ondan evvel en azi 5 simvol olmalidir. abcdfgnail.com meselen)
 
-const validateGmail = (gmail) => gmail.endsWith('@gmail.com') && gmail.length >= 15
+// 1-ci versiya
+// const validateGmail = (gmail) => gmail.endsWith('@gmail.com') && gmail.length >= 15
 
-console.log(validateGmail('ali@gmail.com'))
-console.log(validateGmail('ruslan@gmail.com'));
+// console.log(validateGmail('ali@gmail.com'))
+// console.log(validateGmail('ruslan@gmail.com'));
+
+// _______________________________________________________________________________________________________________________________________
+
+// 2-ci versiya
+
+// let parol = prompt(`parolu daxil et, zehmet olmasa, minumum 6 herfden ibaret olsun, ve parolun icerisinde en azi 1 boyuk, 1 kicik, herf, 1 reqem olmalidir.`)
+
+// const validatePassword = (password) => {
+//     if (parol.length < 6) {
+//         return false
+//     }
+//     let containsNumber = false;
+//     let containsLowerSymbol = false;
+//     let containsUpperSymbol = false;
+//     for (let i = 0; i < password.length; i++) {
+//         const asciiCode = password.charCodeAt(i)
+//         if (48 <= asciiCode && asciiCode <= 57) {
+//             containsNumber = true;
+
+//         }
+
+//         if (65 <= asciiCode && asciiCode <= 90) {
+//             containsUpperSymbol = true;
+
+//         }
+//         if (97 <= asciiCode && asciiCode <= 122) {
+//             containsLowerSymbol = true;
+
+//         }
+//     }
+//     return containsNumber && containsLowerSymbol && containsUpperSymbol
+
+// }
+// console.log(validatePassword(parol));
+
+// _______________________________________________________________________________________________________________________________________
+
+// 3-ci versiya
+// let parol = prompt(`parolu daxil et, zehmet olmasa, minumum 6 herfden ibaret olsun, ve parolun icerisinde en azi 1 boyuk, 1 kicik, herf, 1 reqem olmalidir.`)
+
+// const mediumRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/
+
+// console.log(mediumRegex.test(parol));
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// task-04
+
+// 1-ci versiya
+
+// const bugun = new Date().getDay()
+
+// switch (bugun) {
+//     case 0:
+//         document.write('bazar');
+//         break
+//     case 1:
+//         document.write('bazar ertesi');
+//         break
+//     case 2:
+//         document.write('cersenbe axsami');
+//         break
+//     case 3:
+//         document.write('cersenbe');
+//         break
+//     case 4:
+//         document.write('cume ertesi');
+//         break
+//     case 5:
+//         document.write('cume');
+//         break
+//     case 6:
+//         document.write('senbe');
+//         break
+// }
+
+// _______________________________________________________________________________________________________________________________________
+
+// 2-ci versiya
+
+// const bugun = new Date().getDay()
+// const hefteninGunleri = ['bazar', 'bazar ertesi', 'cersenbe axsami', 'cersenbe', 'cume axsami', 'cume', 'senbe' ]
+// document.write(hefteninGunleri[bugun])
+
+// _______________________________________________________________________________________________________________________________________
+
+
+// 3-ci versiya
+const bugun = new Date().getDay()
+const obj = {
+    0: 'bazar',
+    1: 'bazar ertesi',
+    2: 'cersenbe axsami',
+    3: 'cersenbe',
+    4: 'cume axsami',
+    5: 'cume',
+    6: 'senbe',
+}
+document.write(obj[bugun])
+
+
+
 
 
 
