d61d95dbe7c94cb5786cee3a2a926c6010288c10
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-24 15:42:59 +0400

    reduce- ile tapinmax metodlari

--- L12/home-work5/app.js
+++ L12/home-work5/app.js
@@ -20,26 +20,37 @@
 // Fərqli arqumentlərlə funksiyanı üç dəfə çağırın
 
 // function TheLargestOfTheNumber(a, b, c) {
-
-//     if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+//     let enBooyukReqem = a;
+//      if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
 //         return 'YALNIS REQEM YADA EDED YAZIN!!!!'
 //     }
-//     if (a > b && c < a) {
-//         return a;
-//     } else if (b > a && c < b) {
-//         return b;
-//     } else if (c > b && a < c) {
-//         return c;
-
-//     } else if (a === b || a === c) {
-//         return a
-//     } else if (b === c) {
-//         return b
+//     if (b  > enBooyukReqem) {
+//         enBooyukReqem = b
+//     }else if (c > enBooyukReqem) {
+//         enBooyukReqem = c
 //     }
+//     return enBooyukReqem
+// // ______________________________________________________________________________________________________________________________________
+
+//     // if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+//     //     return 'YALNIS REQEM YADA EDED YAZIN!!!!'
+//     // }
+//     // if (a > b && c < a) {
+//     //     return a;
+//     // } else if (b > a && c < b) {
+//     //     return b;
+//     // } else if (c > b && a < c) {
+//     //     return c;
+
+//     // } else if (a === b || a === c) {
+//     //     return a
+//     // } else if (b === c) {
+//     //     return b
+//     // }
 
 // }
-// console.log(TheLargestOfTheNumber(100, 100, 120));
-// console.log(TheLargestOfTheNumber('Salam', 10, 10));
+// console.log(TheLargestOfTheNumber(140, 100, 120));
+// console.log(TheLargestOfTheNumber('salam', 10, 10));
 
 
 // _________________________________________________________________________________________________________________________________________
@@ -61,7 +72,7 @@
 // Bu halda, funksiya belə mesaj göstərməlidir: Bütün depozitlər üzrə məbləğ: 2700 rubl.
 
 
-
+// let RublUSD = +prompt("dlksdmsd")
 // let RublUSD = parseFloat(prompt('Nece dollar oldugunu yazin'))
 // let USD_Rubl = parseFloat(prompt('Nece Rubl oldugunu yazin'))
 // let result = `Bütün depozitlər üzrə məbləğ: ${RublUSD * 75 + USD_Rubl}`
@@ -76,6 +87,8 @@
 
 // const parametr = (numbers) => numbers; console.log(parametr(Math.min(5, 6, 8, 2)));
 
+// const min = (n1,n2) => Math.min(n1,n2)
+// console.log(min(10,20));
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
 //     Task-05
@@ -140,7 +153,8 @@
 // console.log(isEven(5));
 // console.log(isEven(120));
 
-
+//________________________________________________________________________________________________________________________________________
+// const isEven = number => number % 2 === 0
 // _________________________________________________________________________________________________________________________________________
 
 // ozum ucun bele yazdim tapsiriqdan elave olaraq
@@ -213,7 +227,7 @@
 //     return user === result ? true : false
 // }
 // console.log(users(28));
-// console.log(users(6));
+// console.log(users(6)); 
 // console.log(users(8));
 
 
@@ -245,7 +259,6 @@
 //         case 'fevral':
 //             result = 28
 //         default: {
-//             console.error('YALNIS!!!')
 //             result = 'YALNIS!!!'
 //         }
 //     }
@@ -307,6 +320,7 @@
 //     const numString = String(number)
 //     const numDight = numString.length;
 //     let result = 0;
+
 //     for (let i = 0; i < numDight; i++) {
 //         let digit = Number(numString[i]);
 //         result += Math.pow(digit, numDight);
@@ -318,8 +332,22 @@
 //     }
 // }
 
-// console.log(isArmStrong(153));
-// console.log(isArmStrong(171));
+// console.log(isArmStrong(70));
+// console.log(isArmStrong(371));
+//________________________________________________________________________________________________________________________________________
+// const isArmStrongNumber = (num) => {
+//     let number = String(num)
+//     let pow = number.length;
+//     let cem = 0;
+
+//     for (let i = 0; i < pow; i++) {
+//         cem += number[i] ** pow
+//     }
+//     return cem === num
+// }
+
+// document.write(isArmStrongNumber(71))
+
 
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
@@ -331,12 +359,26 @@
 // const User = (a, b) => {
 //     let result = 0;
 //     for (let i = 0; i < a.length; i++) {
-//         if (a.slice(i, i + b.length) === b){
-//             result++
+//         // if (a.slice(i, i + b.length) === b){
+//         //     result++
+//         // }
+//         // if (a[i] === b) result ++
+//     }
+//     return result
+// }
+// console.log(User('Ruslan Zeynalov Mehman, Qardasi Qehraman', 'a'));
+//________________________________________________________________________________________________________________________________________
+
+// const User = (a, b) => {
+//     let result = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] === b) {
+//             result ++
 //         }
 //     }
 //     return result
 // }
+
 // console.log(User('Ruslan Zeynalov Mehman, Qardasi Qehraman', 'a'));
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
@@ -576,7 +618,15 @@
 //     }
 //     return `${result}`
 // }
-// console.log(cycles(user));
+// console.log(cycles(user))    
+
+
+//________________________________________________________________________________________________________________________________________
+
+
+// let str = '`I know how to use cycles,`'
+// const cycles = (n) => str.repeat(n)
+// console.log(cycles(user))    
 
 // ------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -598,7 +648,7 @@
 // console.log(üçrəqəmliEdedler(190));
 
 
-// ------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
 
 //         Task - 29
@@ -617,7 +667,7 @@
 // console.log(ikiReqemliTekEded(20));
 
 
-// ------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -637,5 +687,343 @@
 //     return result
 // }
 // console.log(ədədinBölünəni(9));
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+// const names = ['ali', 'ruslan', 'ramin', 'sada', 'mahammad', 'sabina', 'idris', 'samil']
+// let max = names[0]
+
+
+// names.forEach((name) => {
+//     if (max.length < name.length) {
+//           max = name
+//     }
+//     return max
+// })
+// console.log(max);
+
+// _________________________________________________________________________________________________________________________________________
+
+// const names = ['ali', 'ruslan', 'ramin', 'sada', 'mahammad', 'sabina', 'idris', 'samil']
+
+// let max = names.reduce((result, name) => {
+//     if (result.length > name.length) {
+//         return result
+//     } else {
+//         return name
+//     }
+
+// })
+// console.log(max);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// const names = ['ali', 'ruslan', 'ramin', 'sada', 'mahammad', 'sabina', 'idris', 'samil']
+// let max = names[0]
+// names.forEach((name) => {
+//     if (name.length < max.length) {
+//         max = name
+//     }
+//     return max
+// })
+// console.log(max);
+
+// _______________________________________________________________________________________________________________________________________
+
+// const names = ['ali', 'ruslan', 'ramin', 'sada', 'mahammad', 'sabina', 'idris', 'samil']
+
+// let min = names.reduce((result, name) => {
+//     if (result.length > name.length) {
+//         return name
+//     } else {
+//         return result
+//     }
+// })
+// console.log(min);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+]
+// users massivinde reduce - dan istifade etmekle
+// T1.her user -in id - lerinin cemini tap;
+
+let ID = users.reduce((result, user) => {
+    return result + user.id
+}, 0)
+console.log(ID);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// T2.en uzun email - i tap;
+
+let email = users.reduce((result, user) => {
+    return result + user.email.length
+}, 0)
+console.log(email);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// T3.en uzun ada malik olan elementi tap;
+
+let uzunAd = users.reduce((result, ad) => {
+    if (result.length > ad.length) {
+        return result.name
+    } else {
+        return ad.name
+    }
+})
+console.log(uzunAd);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// T4.en uzun seher ada malik olan elementi tap;
+
+
+let SeherAd = users.reduce((result, seher) => {
+    if (result.address.city.length > seher.address.city.length) {
+        return result
+    } else {
+        return seher
+    }
+})
+console.log(SeherAd.address.city);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 
 
