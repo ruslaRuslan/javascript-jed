a6439b391ee781e022903838c35045eeb15ad921
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-26 17:36:25 +0400

    add

--- L13-DOM/app.js
+++ L13-DOM/app.js
@@ -2,11 +2,39 @@
 // console.log(document);
 // console.dir(document.body.style.backgroundColor = prompt('isdediyin rengi yaz'))
 
-const rengDeyis = () => {
-    console.dir(document.body.style.backgroundColor = prompt('isdediyin rengi yaz'))   // arxa terefde isdediyimiz rengi prompta yazmaqla teyin edirik
+// -------------------------------------------------------------
+//<button onclick="rengDeyis()">click me</button> asqidakiler buna aitdir
+// const rengDeyis = () => {
+//     console.dir(document.body.style.backgroundColor = prompt('isdediyin rengi yaz'))   // arxa terefde isdediyimiz rengi prompta yazmaqla teyin edirik
 
+// }
+// ---------------------------------------------------------------
+// function kopyala(){
+//     navigator.clipboard.writeText(' RUSLAN ❤ ALI ') //bu teyin etdiyimizi kopyaladi
+// }
+
+
+// console.log(document.getElementById('container'));
+// console.dir(document.getElementById('container'))
+// --------------------------------------------------------------
+// document.getElementById('container').innerHTML = prompt('yaz')
+// ______________________________________________________________
+
+
+// <div id="container">bura yaz</div> asaqidakiler buna aitdir 
+// const container = document.getElementById('container')
+
+// container.innerText = prompt('yaz')
+// container.style.color = prompt('isdediyin rengi tekste teyin et')
+
+// const con = document.querySelector("")   //ilk secdiyi
+// const cons = document.querySelectorAll("") //butun secdikleri
+// input.value = prompt('ad yaz ')
+
+const input = document.querySelector('input')
+const getValue = () => {
+console.log(input.value);
 }
 
-function kopyala(){
-    navigator.clipboard.writeText('RUSLAN ❤ ALI') //bu teyin etdiyimizi kopyaladi
-}
+
+
