ad24ee0d4eb3ff94647cdcf9a34f75d28ef79271
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-02-26 19:03:45 +0400

    kalkulyator getElementById- ile

---
+++ L13-DOM/home-work/app.js
@@ -0,0 +1,27 @@
+const calculyator = () => {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let operator = document.getElementById('operator').value
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default: {
+            result = 'Operator teyin edin'
+        }
+    }
+    alert(result)
+}
+
+
