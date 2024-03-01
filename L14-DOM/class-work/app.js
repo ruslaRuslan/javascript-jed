1d3329d5ef999162d3f4d4867dff5637090010b8
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-01 17:53:07 +0400

    tek js ile sekil elave etdim html-a ve remove()-ile click-i klikliyende silmek funksiyasi yaratdim

--- L14-DOM/class-work/app.js
+++ L14-DOM/class-work/app.js
@@ -19,17 +19,24 @@
 // -------------------------------------------------------------------02-----------------------------------------------------------------
 
 
-const root = document.getElementById('root')
-const button = document.querySelector('button')
+// const root = document.getElementById('root')
+// const button = document.querySelector('button')
 
-button.onclick = () => {
-    root.classList.toggle('red')
-}
-
-
-
+// button.onclick = () => {
+//     root.classList.toggle('red')
+// }
 
 
 // -------------------------------------------------------------------02-----------------------------------------------------------------
 
+const root = document.getElementById('root')
+const button = document.querySelector('button')
 
+const img = document.createElement('img')
+img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/1200px-Lion_waiting_in_Namibia.jpg'
+img.classList.add('lion')
+root.append(img)
+
+button.onclick = () => {
+img.remove()
+}
