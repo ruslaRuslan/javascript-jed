ae4d92e87a1f9aeb6b1693072b2016d7af507442
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-02 16:30:03 +0400

    addEventListener -ile isleme qaydalari

--- L15-DOM/app.js
+++ L15-DOM/app.js
@@ -33,8 +33,42 @@
 
 // -----------------------------------------------------------------------------03----------------------------------------------------
 
-function changeColor(color) {
-    document.body.style.backgroundColor = color
-}
+// function changeColor(color) {
+//     document.body.style.backgroundColor = color
+// }
 
 // -----------------------------------------------------------------------------03----------------------------------------------------
+
+
+// -----------------------------------------------------------------------------04----------------------------------------------------
+
+const btn = document.querySelector('button')
+
+const func = () => {
+    console.log('salam');
+}
+
+
+
+const func2 = () => {
+    alert('salam')
+}
+
+const func3 =  () => { // #jpsd 
+    console.log('heyyy@@22222!!!');
+}
+
+// addEventListener
+btn.addEventListener('click', () => {
+    console.log('heyyy!!!');
+})
+
+btn.addEventListener('click', func3) //#jpsd bura func3 - un referansi elave olunur - (yani func3-e menimsetdiyimiz prosess)
+btn.addEventListener('click', () => {
+    alert('RUSLAN!!!!!!');
+})
+
+btn.removeEventListener('click', func3) //#jpsd bura func3 - un referansi elave olunur - (yani func3-e menimsetdiyimiz prosess)
+
+
+// -----------------------------------------------------------------------------04----------------------------------------------------
