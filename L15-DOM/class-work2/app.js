55a8151007ee8ea510c7dbf430b9eb9851a8f498
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-06 12:09:23 +0400

    setInterval-in islemesi ve button-a klik edende dayanmasi prosesi

--- L15-DOM/class-work2/app.js
+++ L15-DOM/class-work2/app.js
@@ -1,6 +1,17 @@
 // setTimeout -
 // setInterval -
 
-setTimeout(() => {
+// setTimeout(() => {
+//     console.log('Ali');
+// }, 5000) // 5000 miili saniyeden sonra (5 saniyeden sonra ) "Ali" gostereceh
+// // ----------------------------------------------------------------------------------
+
+const id = setInterval(() => {
     console.log('Ali');
-}, 5000) // 5000 miili saniyeden sonra (5 saniyeden sonra ) "Ali" gostereceh
+}, 3000)
+const button = document.querySelector('button')
+
+button.onclick = () => {
+    clearInterval(id)
+}
+// // ----------------------------------------------------------------------------------
