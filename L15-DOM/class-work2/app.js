090f8cdcde84ee0efdc9993c57a52ce891044e81
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-06 13:13:52 +0400

    itemi hem getirdek hem oturduk

--- L15-DOM/class-work2/app.js
+++ L15-DOM/class-work2/app.js
@@ -1,5 +1,5 @@
-// setTimeout -
-// setInterval -
+// setTimeout - bir defe isi gorur 
+// setInterval - her defesinde isi gorur
 
 // setTimeout(() => {
 //     console.log('Ali');
@@ -7,13 +7,37 @@
 
 // // ----------------------------------------------------------------------------------
 
-const id = setTimeout(() => {
-    console.log('Ali');
-}, 3000) //her 3 saniyeden bir "Ali" gostereceh
-const button = document.querySelector('button')
+// const id = setTimeout(() => {
+//     console.log('Ali');
+// }, 3000) //her 3 saniyeden bir "Ali" gostereceh
+// const button = document.querySelector('button')
 
-button.onclick = () => {
-    clearTimeout(id)
-}  //button-a klik edende setInterval-da olan prosesi dayandirir
+// button.onclick = () => {
+//     clearTimeout(id)
+// }  //button-a klik edende setInterval-da olan prosesi dayandirir
 
 // // ----------------------------------------------------------------------------------
+
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+const h1 = document.querySelector('h1')
+const removeBtn = document.querySelector('button')
+const userNameFromLocaleStorage = localStorage.getItem('userName')
+if (userNameFromLocaleStorage) {
+
+    h1.textContent = userNameFromLocaleStorage
+} else {
+    h1.textContent = getName()
+}
+
+
+removeBtn.onclick = () => {
+    localStorage.removeItem('userName')
+}
+
+function getName() {
+
+    const ad = prompt('adi daxi edin')
+    localStorage.setItem("userName", ad)
+    return ad
+}
