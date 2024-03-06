5c656be4f20874d9a7d932f8d0552deac69b542b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-06 14:20:05 +0400

    en boyuk sozu (massivi) tapdim (herif saylarina gore)

--- L15-DOM/for-me2/app.js
+++ L15-DOM/for-me2/app.js
@@ -3,15 +3,33 @@
 
 
 
-const MassivQebulEden = (...massiv) => {
-    let result = []
-    massiv.forEach((element) => {
-        if (element < 0) {
-            result.push(element)
-        }
+// const MassivQebulEden = (...massiv) => {
+//     let result = []
+//     massiv.forEach((element) => {
+//         if (element < 0) {
+//             result.push(element)
+//         }
+//     })
+//     return result
+// }
+// console.log(MassivQebulEden(-5, 25, -2, -85, 32, 20, 14, 123, -1, -57, 57, 85, 89, -97));
+
+// -----------------------------------------------------------------------------------
+
+// Adlar massivini parametr kimi qebul eden bir funksiya yazin, bele ki, o funksiyadan bize en uzun adi cixaracaq. 
+// mes: // let arr = ['ali', 'reza', 'alireza', 'muhammad', 'coshqun', 'eles'] 
+// findLongestName(arr); // 'muhammad'
+
+let words = ['ali', 'reza', 'alireza', 'muhammad', 'coshqun', 'eles']
+// findLongestName(arr); // 'muhammad'
+
+findLongestName = (arr) => {
+    return arr.reduce((result, massiv) => {
+        return result.length > massiv.length ? result : massiv
     })
-    return result
 }
-console.log(MassivQebulEden(-5, 25, -2, -85, 32, 20, 14, 123, -1, -57, 57, 85, 89, -97)); 
+console.log(findLongestName(words));
+
+
 
 // -----------------------------------------------------------------------------------
