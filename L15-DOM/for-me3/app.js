4b503967bac3fad910472ef42de48da0dda3bd1c
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-07 15:42:39 +0400

    throw new Error

--- L15-DOM/for-me3/app.js
+++ L15-DOM/for-me3/app.js
@@ -1,15 +1,16 @@
 
-try {
-    edcvfvf
-} catch (err) {
-    console.log(err.message); //edcvfvf is not defined
-}finally{
+// try {
+//     edcvfvf
+// } catch (err) {
+//     console.log(err.message); //edcvfvf is not defined
+// }finally{
 
-}
+// }
 
-let obj = {
-    name: 'ulvi'
-}
-let str = JSON.stringify(obj)
-console.log(str);
-// --------------------------------------------------------------------------------
+// let obj = {
+//     name: 'ulvi'
+// }
+// let str = JSON.stringify(obj)
+// console.log(str);
+// // --------------------------------------------------------------------------------
+throw new Error('error cixdi')
