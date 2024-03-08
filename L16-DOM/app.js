e2d596e2c1e5548cfb910073fcaf13c953bcf211
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-08 12:37:41 +0400

    obyektleri daha qisa isletme yolu

--- L16-DOM/app.js
+++ L16-DOM/app.js
@@ -1,9 +1,25 @@
-const [first, iki,] = ['ali', 'vali']
-console.log(first);// ali
-console.log(iki);// vali
-// yazilari massive yazdigimiz sira ile saydi ne yazsax siraya gore sayacax
+// const [first, iki,] = ['ali', 'vali']
+// console.log(first);// ali
+// console.log(iki);// vali
+// // yazilari massive yazdigimiz sira ile saydi ne yazsax siraya gore sayacax
 
 
 
-// "destructure" var ve "structure" var
-// bug -> debug
+// // "destructure" var ve "structure" var
+// // bug -> debug
+// // -----------------------------------------------------------------------------------
+
+const [alion, intiqam,] = ['bext qusu', 'vali', 'das']
+
+let human = {
+    ad: 'ali',
+    age: 22
+}
+
+// const age = human.age
+// const ad = human.ad
+
+const { age, ad } = human // yuxaridakilrin evezine belede yazmax olar
+
+console.log(age); // 23
+console.log(ad);  //  ali
