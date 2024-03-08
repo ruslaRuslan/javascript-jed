623ea57a3e101036f4107d67b8941c7981260997
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-08 12:29:24 +0400

    massivin icini ne yazmagimizdan asili olmayaraq sayir

---
+++ L16-DOM/app.js
@@ -0,0 +1,9 @@
+const [first, iki,] = ['ali', 'vali']
+console.log(first);// ali
+console.log(iki);// vali
+// yazilari massive yazdigimiz sira ile saydi ne yazsax siraya gore sayacax
+
+
+
+// "destructure" var ve "structure" var
+// bug -> debug
