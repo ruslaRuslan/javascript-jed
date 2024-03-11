3fe536cdab8cf88fdfd40b62276fc8119cbec706
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-11 11:23:50 +0400

    destructure 2 versiya

--- L16-DOM/class-work/app.js
+++ L16-DOM/class-work/app.js
@@ -1,10 +1,23 @@
-// destructure
+// // destructure
+// const human = {
+//     ad: 'Ruslan',
+//     age: 23
+// }
+
+// function write(obj){
+//     console.log(`${obj.ad} ${obj.age}`);
+// }
+// write(human) //Ruslan 23
+// -----------------------------------------------------------------------------------
+
+// destructure 2 
 const human = {
     ad: 'Ruslan',
-    age: 23
+    age: 23,
+    doing() { }
 }
 
-function write(obj){
-    console.log(`${obj.ad} ${obj.age}`);
+function write({ ad, age }) {
+    console.log(`${ad} ${age}`);
 }
 write(human) //Ruslan 23
