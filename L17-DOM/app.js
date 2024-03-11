eb3bb37315b499ef6ecb35707c690def28ce0579
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-11 14:32:38 +0400

    constructor-un statik metodlari (bele yazmaq tovsiye olunmur)

--- L17-DOM/app.js
+++ L17-DOM/app.js
@@ -47,30 +47,49 @@
 
 // -----------------------------------------------------------------------------------
 
-class Human {
-    constructor(name, surname, age) {
-        this.name = name
-        this.age = age
-        this.surname = surname
-    }
-    tellName = function () {
-        console.log(`My name is ${this.name}`);
-    }
-    hhd() {
-        console.log(`My dear, ${this.name} HBD to you`);
-        this.age++
-    }
-}
-class User extends Human {
-    constructor(username, surname, name, age, isOnline) {
-        super(name, surname, age)
-        this.username = username
-        this.isOnline = this.isOnline
-    }
+// class Human {
+//     constructor(name, surname, age) {
+//         this.name = name
+//         this.age = age
+//         this.surname = surname
+//     }
+//     tellName = function () {
+//         console.log(`My name is ${this.name}`);
+//     }
+//     hhd() {
+//         console.log(`My dear, ${this.name} HBD to you`);
+//         this.age++
+//     }
+// }
+// class User extends Human {
+//     constructor(username, surname, name, age, isOnline) {
+//         super(name, surname, age)
+//         this.username = username
+//         this.isOnline = this.isOnline
+//     }
 
+// }
+// const ali = new User('Ruslan', 'Zeynalov', 'Gahraman', 12, true)
+// ali.tellName() //My name is Gahraman
+// console.log(ali.age);//12
+// ali.hhd()   //My dear, Gahraman HBD to you
+// console.log(ali.age); //13
+
+// -----------------------------------------------------------------------------------
+
+// statik metodlar
+
+class Calculator {
+    constructor(a, b) {
+        this.a = a
+        this.b = b
+    }
+    sum() {
+        console.log(this.a + this.b);
+    }
 }
-const ali = new User('Ruslan', 'Zeynalov', 'Gahraman', 12, true)
-ali.tellName() //My name is Gahraman
-console.log(ali.age);//12
-ali.hhd()   //My dear, Gahraman HBD to you
-console.log(ali.age); //13
+const cal = new Calculator(12, 23)
+cal.sum() //35
+// tovsiye olunmayan kod
+
+// -----------------------------------------------------------------------------------
