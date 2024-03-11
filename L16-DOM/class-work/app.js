76e612f1aad0e3763a3c53eb18eb542ab500fce4
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-11 11:34:59 +0400

    destructure 3 massivde islemesi

--- L16-DOM/class-work/app.js
+++ L16-DOM/class-work/app.js
@@ -10,14 +10,33 @@
 // write(human) //Ruslan 23
 // -----------------------------------------------------------------------------------
 
-// destructure 2 
-const human = {
-    ad: 'Ruslan',
-    age: 23,
-    doing() { }
-}
+// // destructure 2 
+// const human = {
+//     ad: 'Ruslan',
+//     age: 23,
+//     doing() { }
+// }
 
-function write({ ad, age }) {
-    console.log(`${ad} ${age}`);
-}
-write(human) //Ruslan 23
+// function write({ ad, age }) {
+//     console.log(`${ad} ${age}`);
+// }
+// write(human) //Ruslan 23
+
+// -----------------------------------------------------------------------------------
+
+
+// destructure 3
+const fe10 = ['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
+
+const [...rest] = fe10
+console.log(rest); //['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
+// rest -> qalanlari
+// spread -> yaymag
+
+// const ad = fe10[0]
+// console.log(ad); //Ruslan
+// // yuxaridakini evezine asagidakini-de yazmaq olar
+
+// const [sifirinci] = fe10
+// console.log(sifirinci); //Ruslan
+
