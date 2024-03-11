0c1e3789a9ce5b94d081df30a9083bb2635d700b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-11 13:51:23 +0400

    constractor  2 versiya

--- L17-DOM/app.js
+++ L17-DOM/app.js
@@ -4,14 +4,28 @@
 // }
 
 // asagidaki yuxardakiynen eynidi amma bezi usdunlukleri  var (obyekt yonlu proqramlasdirma )
+// class User {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+
+// }
+// const ali = new User('Ruslan', 12)
+// console.log(ali.name); // Ruslan
+// // constractor 
+// // obyekt yaradilan constractor obyektsiz yaratmaq mumkun deyil
+
+// -----------------------------------------------------------------------------------
+
 class User {
     constructor(name, age) {
         this.name = name
         this.age = age
     }
-   
+    tellName() {
+        console.log(`My name is ${this.name}`);
+    }
 }
 const ali = new User('Ruslan', 12)
-console.log(ali.name); // Ruslan
-// constractor 
-// obyekt yaradilan constractor obyektsiz yaratmaq mumkun deyil
+ali.tellName() //My name is Ruslan
