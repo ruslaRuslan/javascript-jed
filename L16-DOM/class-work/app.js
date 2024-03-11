313cabbd5c0032223c538aac7010a2ad703dfbdb
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-11 12:30:30 +0400

    clousure versiya 1

--- L16-DOM/class-work/app.js
+++ L16-DOM/class-work/app.js
@@ -10,7 +10,7 @@
 // write(human) //Ruslan 23
 // -----------------------------------------------------------------------------------
 
-// // destructure 2 
+// // destructure 2
 // const human = {
 //     ad: 'Ruslan',
 //     age: 23,
@@ -26,12 +26,12 @@
 
 
 // destructure 3
-const fe10 = ['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
+// const fe10 = ['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
 
-const [...rest] = fe10
-console.log(rest); //['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
-// rest -> qalanlari
-// spread -> yaymag
+// const [...rest] = fe10
+// console.log(rest); //['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
+// // rest -> qalanlari
+// // spread -> yaymag
 
 // const ad = fe10[0]
 // console.log(ad); //Ruslan
@@ -40,3 +40,23 @@ console.log(rest); //['Ruslan', 'Sada', 'Ali', 'Isa', 'Ramin']
 // const [sifirinci] = fe10
 // console.log(sifirinci); //Ruslan
 
+// -----------------------------------------------------------------------------------
+// IIFE -> immediately invoked function expression
+
+// () => { }
+//1. function(){ }
+//2. yuxaridakiler adlanir Function expression her ikisi-de
+
+// (()=>{})()
+// (() => {
+//     console.log('salam');
+// })() // derhal cagirilma funksiyasi, bunlar IIFE adlani
+
+// // clousure
+// let a = 12;
+
+// (() => {
+//    let b = 9
+//     console.log(a); //12
+// })()
+// console.log(b); //cixmayacax cunki iceriden cole islemir ancax iceriden iceri ve colden iceri isleyir
