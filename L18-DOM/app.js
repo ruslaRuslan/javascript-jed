603a7d037414b93a0a0d99d79aec93332932972c
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-13 16:48:03 +0400

    endpoint* versiya 1

--- L18-DOM/app.js
+++ L18-DOM/app.js
@@ -16,11 +16,32 @@
 // 3.fulffilled -> yerine yetirildi
 
 // endpoint*, api, url, link
-const url = `https://jsonplaceholder.typicode.com/users`
-const data = fetch(url)
-console.log(data);
+// const url = `https://jsonplaceholder.typicode.com/users`
+// const data = fetch(url)
+// console.log(data);
 // bu promise-dir burda isimizi hell etmir
 
+// -------------------------------------------------------------------------------------------------------------
+
+
+// asinxromun icrasi
+// Promise -> soz vermek
+
+// 1. pending -> gozleme
+// 2. rejected-> imtina
+// 3.fulffilled -> yerine yetirildi
+
+// endpoint*, api, url, link
+const url = `https://jsonplaceholder.typicode.com/users`
+fetch(url)
+    .then((respons) => {
+        console.log(respons);
+    })
+    .catch((err) => {
+        console.log('err', err);
+    })
+console.log('ne?');
+// bu promise-dir 
 
 
 
