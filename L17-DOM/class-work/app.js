7fcd5b44181d459f42ccb232813223ee3159220b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-13 15:40:29 +0400

    constructor ile class-i isletmeyin yolu

--- L17-DOM/class-work/app.js
+++ L17-DOM/class-work/app.js
@@ -25,14 +25,14 @@
 
 // -------------------------------------------------------------------------------------------------------------
 
-function createHuman(ad, age) {
-    const obj = {
-        ad,
-        age
-    }
-    return obj
-}
-const ali = createHuman('ali', 21)
+// function createHuman(ad, age) {
+//     const obj = {
+//         ad,
+//         age
+//     }
+//     return obj
+// }
+// const ali = createHuman('ali', 21)
 
 // OOP -> object oriented programming (OYP)-> obyekt yonlu proqramlasdirma
 // class, object (blueprint) -> qelibi
@@ -40,4 +40,31 @@ const ali = createHuman('ali', 21)
 // ruslan, ali -> Insan
 // toplan -> It
 
+// -------------------------------------------------------------------------------------------------------------
+
+class Human {
+    constructor(ad, age, job = 'jobless') {
+        this.ad = ad //this -> bu class-dan torenen yeni obyekti gosterir
+        this.age = age
+        this.job = job
+        this.doing = () => {
+            console.log(`${ad} is coding...`);
+        }
+    }
+
+}
+// OOP -> class hecnedir, class her seydir!
+
+const ali = new Human('ali', 12, 'developer')
+const ruslan = new Human('ruslan', 23, 'student')
+const ramin = new Human('ramin', 14)
+
+console.log(ali); // Human {ad: 'ali', age: 12, doing: ƒ}
+ali.doing() // ali is coding...
+console.log(ruslan); //// Human {ad: 'ruslan', age: 23, doing: ƒ}
+ruslan.doing() // ruslan is coding...
+console.log(ramin) //Human {ad: 'ramin', age: 14, job: 'jobless', doing: ƒ}
+// constructor -> class-dan obyekt yaradir!
+
+// yazdim yazmadim her constuctor-un class-i olacax class-iz constructor yoxdur! (constructor-suz class yoxdur, constructor-suz class hecnedir)
 
