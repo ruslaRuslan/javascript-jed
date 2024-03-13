963951a3d95cfb8b730c5825dfcd191724acd590
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-13 15:03:45 +0400

    funksiya ile olan yolu amma coxda tovsiye olunmayan yol

--- L16-DOM/home-work2/app.js
+++ L16-DOM/home-work2/app.js
@@ -6,11 +6,10 @@ button.showText = () => {
     span.textContent += input + ' '
     document.querySelector('input').value = ''
     span.style.display = 'inline'
-
 }
 
-let button2 = document.getElementById('button2')
 
+let button2 = document.getElementById('button2')
 button2.onclick = () => {
     let span = document.querySelector('span')
     span.textContent = ''
---
+++ L17-DOM/class-work/app.js
@@ -0,0 +1,24 @@
+// eror handling, try/catch, class
+// const ali = {
+//     ad: 'ali',
+//     age: 24
+// }
+
+// const ruslan = {
+//     ad: 'ruslan',
+//     age: 23
+// }
+// const isa = {
+//     ad: 'ali',
+//     age: 26
+// }
+// yuxaridakilerin evezine asagidakileri yazmaz olar
+
+function createHuman(ad, age) {
+    const obj = {
+        ad,
+        age
+    }
+    return obj
+}
+const ali = createHuman('ali', 21)
