e1f95f34f1e8dd26f2c98757fe66ea9a9611a64f
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-13 16:56:50 +0400

    endpoint* promis zenciri - ile versiya 2

--- L18-DOM/app.js
+++ L18-DOM/app.js
@@ -24,6 +24,54 @@
 // -------------------------------------------------------------------------------------------------------------
 
 
+// // asinxromun icrasi
+// // Promise -> soz vermek
+
+// // 1. pending -> gozleme
+// // 2. rejected-> imtina
+// // 3.fulffilled -> yerine yetirildi
+
+// // endpoint*, api, url, link
+// const url = `https://jsonplaceholder.typicode.com/users`
+// fetch(url)
+//     .then((respons) => {
+//         console.log(respons);
+//     })
+//     .catch((err) => {
+//         console.log('err', err);
+//     })
+// console.log('ne?');
+// // bu promise-dir 
+
+// -------------------------------------------------------------------------------------------------------------
+
+
+// // asinxromun icrasi
+// // Promise -> soz vermek
+
+// // 1. pending -> gozleme
+// // 2. rejected-> imtina
+// // 3.fulffilled -> yerine yetirildi
+
+// // endpoint*, api, url, link
+// const url = `https://jsonplaceholder.typicode.com/users`
+// // promise chaining -> promis zenciri
+// fetch(url)
+//     .then((respons) => {
+//         return respons.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return 'Ruslan'
+//     })
+//     .then((ad)=>{
+//         console.log(ad);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// console.log('ne?');
+
 // asinxromun icrasi
 // Promise -> soz vermek
 
@@ -31,19 +79,7 @@
 // 2. rejected-> imtina
 // 3.fulffilled -> yerine yetirildi
 
-// endpoint*, api, url, link
-const url = `https://jsonplaceholder.typicode.com/users`
-fetch(url)
-    .then((respons) => {
-        console.log(respons);
-    })
-    .catch((err) => {
-        console.log('err', err);
-    })
-console.log('ne?');
-// bu promise-dir 
-
-
+// -------------------------------------------------------------------------------------------------------------
 
 
 
