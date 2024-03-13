224f8c8b644487f89dc43d07c41a95cdc2e4e5c7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-13 16:38:11 +0400

    asinxromun icrasi bu promise-dir

--- L18-DOM/app.js
+++ L18-DOM/app.js
@@ -1,8 +1,28 @@
-setTimeout(() => {
+// setTimeout(() => {
 
-    console.log('a')
-}, 1);
-console.log('b')
-console.log('c')
+//     console.log('a')
+// }, 1);
+// console.log('b')
+// console.log('c')
+
+// asinxrom
+// -------------------------------------------------------------------------------------------------------------
+
+// asinxromun icrasi
+// Promise -> soz vermek
+
+// 1. pending -> gozleme
+// 2. rejected-> imtina
+// 3.fulffilled -> yerine yetirildi
+
+// endpoint*, api, url, link
+const url = `https://jsonplaceholder.typicode.com/users`
+const data = fetch(url)
+console.log(data);
+// bu promise-dir burda isimizi hell etmir
+
+
+
+
 
 
