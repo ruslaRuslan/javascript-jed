2db552e1781ab5236376f56c6f7f21fb10fe8a51
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-14 13:30:59 +0400

    open, send, onreadystatechange funksiyasi ile

--- L17-DOM/for-me2/app.js
+++ L17-DOM/for-me2/app.js
@@ -18,22 +18,37 @@
 // -------------------------------------------------------------------------------------------------------------
 
 
-const obj = {
-    ad: 'ruslan',
-    yas: 23,
-    doing: () => {
-        console.log('salam');
-    },
-    car: undefined,
-    test: null
+// const obj = {
+//     ad: 'ruslan',
+//     yas: 23,
+//     doing: () => {
+//         console.log('salam');
+//     },
+//     car: undefined,
+//     test: null
+// }
+
+// // obj -> json -> newObj
+// // doing? car? test?
+// const json = JSON.stringify(obj)
+// console.log(json);
+
+// const newObj = JSON.parse(json)
+// console.log(newObj);
+// -------------------------------------------------------------------------------------------------------------
+
+// RESTful -> REST API
+
+// AJAX -> Asynchronous JavaScript And Xml
+
+const url = `https://jsonplaceholder.typicode.com/users`
+const xhr = new XMLHttpRequest()
+
+xhr.open('GET', url)
+xhr.send()
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+        console.log(xhr.response);
+    }
+    
 }
-
-// obj -> json -> newObj
-// doing? car? test?
-const json = JSON.stringify(obj)
-console.log(json);
-
-const newObj = JSON.parse(json)
-console.log(newObj);
-
-
