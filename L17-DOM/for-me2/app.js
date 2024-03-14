497a94405e15133fa0c1de6131888c1e2ad6110c
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-14 12:39:16 +0400

    JSON parse-ile islemesi

--- L17-DOM/for-me2/app.js
+++ L17-DOM/for-me2/app.js
@@ -1,9 +1,19 @@
 // JavaScript Object Notation
+// const ad = prompt('adini daxil et')
+// const yas = +prompt('yasini daxil et')
+// const obj = { ad, yas }
+// const json = JSON.stringify(obj)
+// console.log(json);
+
+
+// -------------------------------------------------------------------------------------------------------------
+
 const ad = prompt('adini daxil et')
 const yas = +prompt('yasini daxil et')
 const obj = { ad, yas }
-const json = JSON.stringify(obj)
-console.log(json);
+const jsonFromBackend = `{"ad":"ruslan","yas":23}`
+const human = JSON.parse(jsonFromBackend)
+console.log(human);
 
 
 
