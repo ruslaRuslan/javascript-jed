383dd1ac2c7682eedbd3ec96ea2f7b51ba1d1896
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-14 12:50:26 +0400

    JSON.stringify && JSON.parse

--- L17-DOM/for-me2/app.js
+++ L17-DOM/for-me2/app.js
@@ -8,14 +8,32 @@
 
 // -------------------------------------------------------------------------------------------------------------
 
-const ad = prompt('adini daxil et')
-const yas = +prompt('yasini daxil et')
-const obj = { ad, yas }
-const jsonFromBackend = `{"ad":"ruslan","yas":23}`
-const human = JSON.parse(jsonFromBackend)
-console.log(human);
+// const ad = prompt('adini daxil et')
+// const yas = +prompt('yasini daxil et')
+// const obj = { ad, yas }
+// const jsonFromBackend = `{"ad":"ruslan","yas":23}`
+// const human = JSON.parse(jsonFromBackend)
+// console.log(human);
+
+// -------------------------------------------------------------------------------------------------------------
 
 
+const obj = {
+    ad: 'ruslan',
+    yas: 23,
+    doing: () => {
+        console.log('salam');
+    },
+    car: undefined,
+    test: null
+}
 
+// obj -> json -> newObj
+// doing? car? test?
+const json = JSON.stringify(obj)
+console.log(json);
+
+const newObj = JSON.parse(json)
+console.log(newObj);
 
 
