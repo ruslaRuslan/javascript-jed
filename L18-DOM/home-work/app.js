81ac08bf5b796574990bda17d8c630d58e534ecc
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-15 16:03:55 +0400

    istifadecinin id-lerini ve name-lerini cixartdim

--- L18-DOM/home-work/app.js
+++ L18-DOM/home-work/app.js
@@ -13,16 +13,33 @@
 // -------------------------------------------------------------------------------------------------------------
 
 // istifadecinin username-lerini cixartmaliyiq
-const url = `https://jsonplaceholder.typicode.com/users`
+// const url = `https://jsonplaceholder.typicode.com/users`
 
+// const root = document.getElementById('root')
+// fetch(url)
+//     .then((respons) => respons.json())
+//     .then((data) => {
+//         data.forEach((user) => {
+//             const p = document.createElement('p')
+//             p.textContent = user.username
+//             root.appendChild(p)
+//         });
+//     })
+
+// -------------------------------------------------------------------------------------------------------------
+
+// istifadecinin id-lerini ve name-lerini cixartmaliyiq
+
+const url = `https://jsonplaceholder.typicode.com/users`
 const root = document.getElementById('root')
 fetch(url)
     .then((respons) => respons.json())
     .then((data) => {
         data.forEach((user) => {
             const p = document.createElement('p')
-            p.textContent = user.username
+            p.textContent = `${user.id}  ${user.name}`
             root.appendChild(p)
         });
     })
+
 
