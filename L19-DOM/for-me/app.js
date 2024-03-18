dc148b889c9bdde5e17feb4b22748cbcbfc42ffb
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-18 14:47:25 +0400

    meals

--- L19-DOM/for-me/app.js
+++ L19-DOM/for-me/app.js
@@ -1,15 +1,28 @@
+// // json file istifade
+// // json stringify, parse
+// // fetch
+
+// const obj = {
+//     ad: 'Ruslan',
+//     yas: 23
+// }
+// JSON.stringify(obj)
+// const json_variant = `{"ad":"Ruslan", "yas":23}`
+
+// // clear clarify
+// // minimum minify
+// // sring stringify
+// ---------------------------------------------------------------------------------------------------------
+
+
+
 // json file istifade
 // json stringify, parse
 // fetch
 
-const obj = {
-    ad: 'Ruslan',
-    yas: 23
-}
-JSON.stringify(obj)
-const json_variant = `{"ad":"Ruslan", "yas":23}`
-
-// clear clarify
-// minimum minify
-// sring stringify
-
+const url = `https://www.themealdb.com/api/json/v1/1/random.php`
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
