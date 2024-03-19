638b9ed4c92162e2a276a7e5467a474000b966c4
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-19 11:46:43 +0400

    Response

--- L20-DOM/for-me/app.js
+++ L20-DOM/for-me/app.js
@@ -12,7 +12,19 @@
 // 2. rejected  -> imtina
 // 3. fulfilled -> yerine yetirildi
 
-// endpoint*, api, url, link
-const  url = `https://jsonplaceholder.typicode.com/users`
-const data = fetch(url)
-console.log(data);
+// endpoint *, api, url, link
+// const url = `https://jsonplaceholder.typicode.com/users`
+// const data = fetch(url)
+// console.log(data);
+// -----------------------------------------------------------------------------------------------------------
+
+const url = `https://jsonplaceholder.typicode.com/users`
+fetch(url)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log('error', error);
+    })
+console.log('ne?'); // "ne?" asinxron-du
+// resolve -> hell etmek
