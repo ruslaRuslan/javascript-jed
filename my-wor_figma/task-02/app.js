649599ab67eb8f477ee68a51042105a26294990d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-21 16:38:01 +0400

    add

---
+++ my-wor_figma/task-02/app.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Python Developer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
