e28b40230c06c9c23236d876c2790f32c174cc30
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-21 12:42:50 +0400

    data-lari cixartdim

--- L19-DOM/home-work/app.js
+++ L19-DOM/home-work/app.js
@@ -18,40 +18,63 @@
 // https://www.themealdb.com/api/json/v1/1/random.php
 // random yemek resepti veren api. Bundan istifade ederek, yemek haqqinda melumatlar verin ))
 
-const url = `https://www.themealdb.com/api/json/v1/1/random.php`
-const getRecipeButton = document.getElementById('getRecipeButton')
-const recipeDisplay = document.getElementById('recipeDisplay')
-getRecipeButton.addEventListener('click', function () {
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const meal = data.meals[0]
-            recipeDisplay.innerHTML = `
-            <h2> ${meal.strMeal} </h2>
-            <p> <strong>Kategori:</strong> ${meal.strCategory} </p>
-            <p> <strong>MATERIALS:</strong> </p>
-            <ul> 
-            ${getIngredients(meal).join('')}
-            </ul>
-             <p><strong> Specification: </strong> ${meal.strInstructions} </p>
-            `
-        })
-        .catch((error) => {
-            console.log('error', error);
-        })
+// const url = `https://www.themealdb.com/api/json/v1/1/random.php`
+// const getRecipeButton = document.getElementById('getRecipeButton')
+// const recipeDisplay = document.getElementById('recipeDisplay')
+// getRecipeButton.addEventListener('click', function () {
+//     fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             const meal = data.meals[0]
+//             recipeDisplay.innerHTML = `
+//             <h2> ${meal.strMeal} </h2>
+//             <p> <strong>Kategori:</strong> ${meal.strCategory} </p>
+//             <p> <strong>MATERIALS:</strong> </p>
+//             <ul> 
+//             ${getIngredients(meal).join('')}
+//             </ul>
+//              <p><strong> Specification: </strong> ${meal.strInstructions} </p>
+//             `
+//         })
+//         .catch((error) => {
+//             console.log('error', error);
+//         })
 
-})
+// })
 
-const getIngredients = (meal) => {
-    const ingredients = []
-    for (let i = 1; i <= 20; i++) {
-        const ingredient = meal[`strIngredient${i}`]
-        if (ingredient) {
-            const measure = meal[`strMeasure${i}`]
-            ingredients.push(`<li>${ingredient} - ${measure}</li>`)
-        } else {
-            break
-        }
-    }
-    return ingredients
-}
+// const getIngredients = (meal) => {
+//     const ingredients = []
+//     for (let i = 1; i <= 20; i++) {
+//         const ingredient = meal[`strIngredient${i}`]
+//         if (ingredient) {
+//             const measure = meal[`strMeasure${i}`]
+//             ingredients.push(`<li>${ingredient} - ${measure}</li>`)
+//         } else {
+//             break
+//         }
+//     }
+//     return ingredients
+// }
+
+
+// -------------------------------------------------------------------------------------------------------------
+// StarWars apisinden insanlar (people) datalari çəkin və göstərin.
+
+const url = `https://swapi.dev/api/people/`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const people = data.results;
+
+        people.forEach(person => {
+            document.write(`${person.name} </br>`);
+            document.write(`${person.birth_year} </br>`);
+            document.write(`${person.gender} </br>`);
+            document.write(`${person.eye_color} </br>`);
+            document.write('----------------------------------------------------------------------------------------------------------------------------- </br>');
+        });
+    })
+    .catch((error)=>{
+        console.log('error', error);
+    })
