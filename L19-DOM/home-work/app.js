f9d23586324a5edcc8e0f098e2e647a794b838a6
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-21 12:19:23 +0400

    yemek resepti

--- L19-DOM/home-work/app.js
+++ L19-DOM/home-work/app.js
@@ -1,13 +1,57 @@
 // get random task adli button yaradin. Ona clickleyende https://catfact.ninja/fact apisinden pishikler haqqinda random fact getirib, ikinci hisseye yazdirmalisiniz.  Dizayna cox da onem vermeyin
 
-const url = `https://catfact.ninja/fact`
+// const url = `https://catfact.ninja/fact`
+// document.getElementById('getFactButton').addEventListener('click', function () {
+//     fetch(url).then((response) => response.json())
+//         .then((data) => {
+//             document.getElementById('factDisplay').innerHTML = data.fact
+//         })
+//         .catch((error) => {
+//             console.log('error', error);
+//         })
+// })
+// -------------------------------------------------------------------------------------------------------------
 
-document.getElementById('getFactButton').addEventListener('click', function () {
-    fetch(url).then((response) => response.json())
+
+
+
+// https://www.themealdb.com/api/json/v1/1/random.php
+// random yemek resepti veren api. Bundan istifade ederek, yemek haqqinda melumatlar verin ))
+
+const url = `https://www.themealdb.com/api/json/v1/1/random.php`
+const getRecipeButton = document.getElementById('getRecipeButton')
+const recipeDisplay = document.getElementById('recipeDisplay')
+getRecipeButton.addEventListener('click', function () {
+    fetch(url)
+        .then((response) => response.json())
         .then((data) => {
-            document.getElementById('factDisplay').innerHTML = data.fact
+            const meal = data.meals[0]
+            recipeDisplay.innerHTML = `
+            <h2> ${meal.strMeal} </h2>
+            <p> <strong>Kategori:</strong> ${meal.strCategory} </p>
+            <p> <strong>MATERIALS:</strong> </p>
+            <ul> 
+            ${getIngredients(meal).join('')}
+            </ul>
+             <p><strong> Specification: </strong> ${meal.strInstructions} </p>
+            `
         })
         .catch((error) => {
             console.log('error', error);
         })
+
 })
+
+const getIngredients = (meal) => {
+    const ingredients = []
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`]
+        if (ingredient) {
+            const measure = meal[`strMeasure${i}`]
+            ingredients.push(`<li>${ingredient} - ${measure}</li>`)
+        } else {
+            break
+        }
+    }
+    return ingredients
+}
